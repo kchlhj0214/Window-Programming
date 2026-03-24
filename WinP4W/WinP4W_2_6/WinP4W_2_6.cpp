9ff2db3c38f11766c80bec2d94d289d6c0807035
kchlhj0214 <wlsdlf117@naver.com>
2026-03-24 18:58:45 +0900

    2-6
    2-6

--- WinP4W/WinP4W_2_6/WinP4W_2_6.cpp
+++ WinP4W/WinP4W_2_6/WinP4W_2_6.cpp
@@ -4,14 +4,13 @@
 #include <vector>
 
 using namespace std;
-random_device rd;
-mt19937 g(rd());
-uniform_int_distribution<> uid{ 0, 600 };		// 800 600 기준 12단까지
-random_device rd2;
-mt19937 g2(rd2());
-uniform_int_distribution<> uid2{ 0, 255 };
 #define LEN 800
 #define HEI 600
+random_device rd;
+mt19937 g(rd());
+uniform_int_distribution<> uid{ 0, LEN - 200 };		// 800 600 기준 12단까지
+uniform_int_distribution<> uid2{ 0, 255 };
+
 
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = L"My Window Class";
@@ -48,28 +47,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
 	return Message.wParam;
 }
 
+struct NumRect {
+	int x, y, n, count;
+};
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
 	HDC hDC;
-	static TCHAR str[5][21] = { 0 };
-	static int count = 0;
-	static int yPos = 0;
-	static SIZE size;
-	static int line = 0;
-	static int x, y;
+	static TCHAR str[20] = { 0 };
+	static TCHAR lpOut[20] = { 0 };
+	static vector<COLORREF> colors;
+	int rgbnum = 0;
+	static int idx = 0;
+	static NumRect rects[10];
+	static int rectCount = 0;
 	static BYTE r_c, g_c, b_c;
-
-
+	static int prtChoice = 0;		// 0 = e일경우(단일 출력), 1 = a일경우(모두 출력), 2 = 엔터일경우(출력 없이 첫 대기화면으로)
 
 	switch (uMsg) {
 	case WM_CREATE:
-		x = uid(g);
-		y = uid(g) % 500;
-		r_c = (BYTE)uid2(g2);
-		g_c = (BYTE)uid2(g2);
-		b_c = (BYTE)uid2(g2);
+		for (int i = 0; i < 30; ++i)
+			colors.push_back(RGB(uid2(g), uid2(g), uid2(g)));
 		CreateCaret(hWnd, NULL, 2, 15);
 		ShowCaret(hWnd);
 		break;
@@ -77,55 +76,72 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		hDC = GetDC(hWnd);
 
 		if (wParam == 'e') {					// 4개 숫자 입력 후 사각형 생성
-			if (count > 0) {
-				--count;
-				str[line][count] = '\0';
+			if (rectCount < 10) {
+				int tx, ty, tn, tc;
+				if (_stscanf_s(str, _T("%d %d %d %d"), &tx, &ty, &tn, &tc) == 4) {
+					rects[rectCount].x = tx;
+					rects[rectCount].y = ty;
+					rects[rectCount].n = tn;
+					rects[rectCount].count = tc;
+					++rectCount;
+					prtChoice = 0;
+				}
 			}
-			else if (line > 0) {
-				--line;
-				count = lstrlen(str[line]);
-			}
+			idx = 0;
+			memset(str, 0, sizeof(str));
+			InvalidateRect(hWnd, NULL, TRUE);
 		}
-		else if (wParam == VK_RETURN) {			// 기존 사각형 지운 후 새롭게 4개 숫자 입력
-			if (yPos + ((line + 1) * 20) + y < HEI - 50)
-				yPos += 20;
+		else if (wParam == VK_RETURN) {			// 기존 사각형 지운 후 새롭게 4개 숫자 입력대기
+			prtChoice = 2;
+			idx = 0;
+			memset(str, 0, sizeof(str));
+			InvalidateRect(hWnd, NULL, TRUE);
 		}
 		else if (wParam == 'q') {				// 프로그램 종료
 			PostQuitMessage(0);
 		}
 		else if (wParam == 'a') {				// 그동안 출력한 문자들 전부 출력
-			PostQuitMessage(0);
+			prtChoice = 1;
+			InvalidateRect(hWnd, NULL, TRUE);
 		}
 		else if (wParam == 'r') {				// 모두 리셋하고 처음부터 시작
-			PostQuitMessage(0);
+			rectCount = 0;
+			prtChoice = 2;
+			rgbnum = 0;
+			for (int i = 0; i < 30; ++i)
+				colors.push_back(RGB(uid2(g), uid2(g), uid2(g)));
+			idx = 0;
+			memset(str, 0, sizeof(str));
+			InvalidateRect(hWnd, NULL, TRUE);
 		}
-		else {
-			if (count < 20) {
-				str[line][count++] = wParam;
-				str[line][count] = '\0';
-
-			}
-			else {
-				if (line < 4 && yPos + ((line + 1) * 20) + y < HEI - 50) {	// yPos + ((line + 1) * 20) + y < HEI - 50 이 조건이 없다면 현재 입력된 문자열의 끝 줄이 맨 아래까지 이동하더라도 총 줄의 수가 5보다 작다면 화면 밖에 문자열을 추가함
-					count = 0;
-					++line;
-					str[line][count++] = wParam;
-					str[line][count] = '\0';
-				}
+		else { // 일반 문자 입력
+			if (idx < 20) {
+				str[idx++] = (TCHAR)wParam;
+				str[idx] = '\0';
 			}
 		}
-
-		str[line][count] = '\0';
 		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 	case WM_PAINT:
 		hDC = BeginPaint(hWnd, &ps);
-		SetTextColor(hDC, RGB(r_c, g_c, b_c));
-		for (int i = 0; i <= line; ++i) {
-			TextOut(hDC, x, yPos + (i * 20) + y, str[i], lstrlen(str[i]));
+		
+		if (rectCount > 0) {
+			if (prtChoice == 1) {
+				for (int i = 0; i < rectCount - 1; i++) {
+					SetBkMode(hDC, OPAQUE);
+					SetBkColor(hDC, colors[rgbnum++]);
+					SetTextColor(hDC, colors[rgbnum++]);
+
+					for (int j = 0; j < rects[i].count; ++j) 
+						wsprintf(lpOut, L"%d", rects[i].n);
+
+					for (int j = 0; j < rects[i].count; ++j) 
+						TextOut(hDC, rects[i].x, rects[i].y, lpOut, lstrlen(lpOut));
+				}
+			}
+			TextOut(hDC, rects[rectCount].x, rects[rectCount].y, lpOut, lstrlen(lpOut));
 		}
-		GetTextExtentPoint32(hDC, str[line], lstrlen(str[line]), &size);
-		SetCaretPos(size.cx + x, yPos + y + (line * 20));
+
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
