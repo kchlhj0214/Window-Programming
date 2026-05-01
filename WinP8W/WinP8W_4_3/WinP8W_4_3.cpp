91562fc058eafdf224c2267a8a388f8178bf8668
kchlhj0214 <wlsdlf117@naver.com>
2026-05-02 03:59:49 +0900

    하...
    꺾인다

--- WinP8W/WinP8W_4_3/WinP8W_4_3.cpp
+++ WinP8W/WinP8W_4_3/WinP8W_4_3.cpp
@@ -13,7 +13,10 @@
 #define START_X 100
 #define START_Y 20
 #define PADDLE_LEN 150
-#define PADDLE_HEI 10
+#define PADDLE_HEI 15
+#define BALL_RADIUS 10
+#define MAX_SPEED 10.0f
+#define MIN_SPEED 3.0f
 
 using namespace std;
 random_device rd;
@@ -32,10 +35,14 @@ LPCTSTR lpszClass = L"My Window Class";
 LPCTSTR lpszWindowName = L"windows program 2";
 
 PADDLE paddle;
-BRICK bricks[BRICK_NUM][BRICK_LINE];
+BRICK bricks[BRICK_LINE][BRICK_NUM];
 BALL ball;
 int colorChangedBricks = 0;
 int brokenBricks = 0;
+float lastPaddleX = 0.0f;
+bool isPaddleDragging = false;
+bool isP = false;
+bool isS = false;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
 
@@ -71,9 +78,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
 void init_setting()
 {
 	// 벽돌 세팅
-	for (int i = 0; i < BRICK_NUM; ++i) {
+	for (int i = 0; i < BRICK_LINE; ++i) {
 		int dir = uid_dir(g);
-		for (int j = 0; j < BRICK_LINE; ++j) {
+		for (int j = 0; j < BRICK_NUM; ++j) {
 			bricks[i][j].color = RGB(255, 255, 0);
 			bricks[i][j].IsFalling = false;
 			bricks[i][j].line = i;
@@ -84,23 +91,77 @@ void init_setting()
 	}
 
 	// 패들 세팅
-	paddle.pos.x = LEN / 2;
-	paddle.pos.y = HEI - 100;
+	paddle.pos.x = LEN / 2;		// 중심의 x좌표
+	paddle.pos.y = HEI - 100;	// 윗부분 y좌표
 	paddle.velocity = 0;
 
 	// 공 세팅
 	ball.pos.x = paddle.pos.x;
-	ball.pos.y = paddle.pos.y - (PADDLE_HEI / 2);
+	ball.pos.y = paddle.pos.y - BALL_RADIUS;
 	ball.moving_pos.x = 0;
 	ball.moving_pos.y = 0;
 
 	colorChangedBricks = 0;
 	brokenBricks = 0;
+
+	colorChangedBricks = 0;
+	brokenBricks = 0;
+	lastPaddleX = 0.0f;
+	isPaddleDragging = false;
+	isP = false;
+	isS = false;
 }
 
-void CollisionToPaddle() 
+void CollisionToPaddle()
 {
+	int r = BALL_RADIUS;
+	RECT bRect = { ball.pos.x - r, ball.pos.y - r , ball.pos.x + r , ball.pos.y + r };
+	RECT pRect = { paddle.pos.x - PADDLE_LEN / 2, paddle.pos.y, paddle.pos.x + PADDLE_LEN / 2, paddle.pos.y + PADDLE_HEI };
+	RECT temp;
+	if (IntersectRect(&temp, &bRect, &pRect)) {
+		if (ball.moving_pos.y > 0) {
+			// --- [1] 반사각 결정 로직 ---
+			// 패들의 중심에서 공이 얼마나 떨어져 있는지 비율 계산 (-1.0 ~ 1.0)
+			float hitPoint = (ball.pos.x - paddle.pos.x) / (PADDLE_LEN / 2.0f);
 
+			// hitPoint에 따라 X축 속도를 결정 (가장자리일수록 X값이 커져서 옆으로 누움)
+			// 최대 X속도를 10.0f 정도로 잡았을 때:
+			ball.moving_pos.x = hitPoint * 10.0f;
+
+			// Y축은 항상 위로 튕겨나가되, X가 커질수록(옆으로 누울수록) 작아져야 자연스러움
+			// 피타고라스 정리에 의해 일정한 전체 속력을 유지하도록 설정하거나, 
+			// 간단하게 고정값(단, 위쪽 방향이므로 음수)을 줍니다.
+			ball.moving_pos.y = -sqrt(max(0.0f, 100.0f - pow(ball.moving_pos.x, 2))); // 전체 속력 10 유지
+
+			// --- [2] 패들 속도에 따른 가속/감속 로직 ---
+			// 패들이 느리면(예: 5 미만) 감속, 빠르면(예: 15 이상) 가속
+			float speedFactor = 1.0f;
+			if (paddle.velocity < 10) {
+				speedFactor = 0.8f;  // 살짝 갖다 대면 속도 감소
+			}
+			else if (paddle.velocity > 25) {
+				speedFactor = 1.3f;  // 강하게 휘두르면 속도 증가
+			}
+
+			ball.moving_pos.x *= speedFactor;
+			ball.moving_pos.y *= speedFactor;
+
+			// 위치 보정 (패들에 공이 박히는 현상 방지)
+			ball.pos.y = paddle.pos.y - r;
+		}
+	}
+	// 현재 전체 속력(크기) 계산
+	float currentSpeed = sqrt(pow(ball.moving_pos.x, 2) + pow(ball.moving_pos.y, 2));
+
+	// 최대/최소 속도 제한 적용
+	if (currentSpeed > MAX_SPEED) {
+		ball.moving_pos.x = (ball.moving_pos.x / currentSpeed) * MAX_SPEED;
+		ball.moving_pos.y = (ball.moving_pos.y / currentSpeed) * MAX_SPEED;
+	}
+	else if (currentSpeed < MIN_SPEED) {
+		ball.moving_pos.x = (ball.moving_pos.x / currentSpeed) * MIN_SPEED;
+		ball.moving_pos.y = (ball.moving_pos.y / currentSpeed) * MIN_SPEED;
+	}
 }
 
 void CollisionToBrick()
@@ -120,12 +181,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 	switch (uMsg) {
 	case WM_CREATE:
+		init_setting();
+		SetTimer(hWnd, 0, 10, NULL);
 		break;
 	case WM_KEYDOWN:
-		if (wParam == 'M') {
-			mode = (mode == 0) ? 1 : 0;
-			isDragging = false;
+		if (wParam == 'S') {
+			if (!isS) {
+				isS = true;
+				ball.moving_pos.x = 0;
+				ball.moving_pos.y = -7.0f;
+			}
+		}
+		else if (wParam == 'P') {
+			if (isP && isS)
+				SetTimer(hWnd, 0, 10, NULL);
+			else if (!isP && isS)
+				KillTimer(hWnd, 0);
+			isP = !isP;
 			InvalidateRect(hWnd, NULL, FALSE);
+		}
+		else if (wParam == 'T') {
+
+			InvalidateRect(hWnd, NULL, FALSE);
+		}
+		else if (wParam == VK_OEM_PLUS || wParam == VK_ADD) {
+
+			InvalidateRect(hWnd, NULL, FALSE);
+		}
+		else if (wParam == VK_OEM_MINUS || wParam == VK_SUBTRACT) {
+
+			InvalidateRect(hWnd, NULL, FALSE);
+		}
+		else if (wParam == 'R') {
+			KillTimer(hWnd, 0);
+
+			isS = false;
+			isP = false;
+			init_setting();
+
+			SetTimer(hWnd, 0, 10, NULL);
+			InvalidateRect(hWnd, NULL, FALSE);
+		}
+		else if (wParam == 'Q' || wParam == VK_ESCAPE) {
+			KillTimer(hWnd, 0);
+			PostQuitMessage(0);
 		}
 		break;
 	case WM_SIZE:
@@ -151,187 +250,41 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			// 배경 지우기 (더블 버퍼링의 핵심: 하얀색으로 백버퍼를 채움)
 			FillRect(memDC, &ps.rcPaint, (HBRUSH)GetStockObject(WHITE_BRUSH));
 
-			HBRUSH hBgBrush = CreateSolidBrush(RGB(245, 245, 245));
-			FillRect(memDC, &rt, hBgBrush);
-			DeleteObject(hBgBrush);
+			// --- 1. 벽돌 그리기 ---
+			for (int i = 0; i < BRICK_LINE; ++i) {
+				for (int j = 0; j < BRICK_NUM; ++j) {
+					// 파괴되지 않은 벽돌만 그림 (추후 CollisionToBrick에서 IsFalling 등을 활용할 수 있음)
+					if (!bricks[i][j].IsFalling) {
+						hBrush = CreateSolidBrush(bricks[i][j].color);
+						oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
 
-			HBRUSH hBoardBrush = CreateSolidBrush(RGB(255, 255, 255));
+						Rectangle(memDC, (int)bricks[i][j].pos.x, (int)bricks[i][j].pos.y, (int)bricks[i][j].pos.x + BRICK_LEN, (int)bricks[i][j].pos.y + BRICK_HEI);
 
-			for (int i = 0; i < board_len; ++i) {
-				for (int j = 0; j < board_hei; ++j) {
-					Rectangle(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE);
-					if (opened_board[i][j] == 0) {
-						HBRUSH hBrush = CreateSolidBrush(RGB(200, 200, 200));
-						HBRUSH oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
-						Rectangle(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE);
 						SelectObject(memDC, oldBrush);
 						DeleteObject(hBrush);
 					}
-					else if (opened_board[i][j] == 1) {
-						int x1 = START_X + j * CELL_SIZE;
-						int y1 = START_Y + i * CELL_SIZE;
-						int x2 = x1 + CELL_SIZE;
-						int y2 = y1 + CELL_SIZE;
-						int midX = x1 + CELL_SIZE / 2;
-						int midY = y1 + CELL_SIZE / 2;
-						if (closed_board[i][j] == 1) {
-							SetTextColor(memDC, RGB(255, 0, 0));
-							SetBkMode(memDC, TRANSPARENT);
-
-							HFONT hFont = CreateFont(CELL_SIZE * 0.75, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
-								DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-								DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial");
-							HFONT oldFont = (HFONT)SelectObject(memDC, hFont);
-
-							UINT oldAlign = SetTextAlign(memDC, TA_CENTER | TA_BASELINE);
-
-							TextOut(memDC, midX, midY + (int)(CELL_SIZE * 0.25), L"B", 1);
-
-							SetTextAlign(memDC, oldAlign);
-							SelectObject(memDC, oldFont);
-							DeleteObject(hFont);
-						}
-						else if (closed_board[i][j] == 2) {
-							HBRUSH hItemBrush = CreateSolidBrush(RGB(255, 255, 0));
-							HBRUSH oldItemBrush = (HBRUSH)SelectObject(memDC, hItemBrush);
-
-							POINT pts[4] = { { midX, y1 }, { x2, midY }, { midX, y2 }, { x1, midY } };
-
-							Polygon(memDC, pts, 4);
-
-							SelectObject(memDC, oldItemBrush);
-							DeleteObject(hItemBrush);
-						}
-						else if (closed_board[i][j] > 2) {
-							int val = closed_board[i][j] - 3;
-							int remainder = val % 4;
-							int colorIdx = val / 4;
-
-							HBRUSH hBrush = CreateSolidBrush(pie_color[colorIdx]);
-							HBRUSH oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
-
-							if (remainder == 0) Pie(memDC, x1, y1, x2, y2, midX, y1, x1, midY);
-							else if (remainder == 1) Pie(memDC, x1, y1, x2, y2, x2, midY, midX, y1);
-							else if (remainder == 2) Pie(memDC, x1, y1, x2, y2, midX, y2, x2, midY);
-							else if (remainder == 3) Pie(memDC, x1, y1, x2, y2, x1, midY, midX, y2);
-
-							SelectObject(memDC, oldBrush);
-							DeleteObject(hBrush);
-							/*if ((closed_board[i][j] - 3) % 4 == 0) {
-								HBRUSH hBrush = CreateSolidBrush(pie_color[(closed_board[i][j] - 3) / 4]);
-								Pie(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE,
-									START_X + (j + 1) * (CELL_SIZE / 2), START_Y + i * CELL_SIZE, START_X + j * CELL_SIZE, START_Y + (i + 1) * (CELL_SIZE / 2));
-								DeleteObject(hBrush);
-							}
-							else if((closed_board[i][j] - 3) % 4 == 1) {
-								HBRUSH hBrush = CreateSolidBrush(pie_color[(closed_board[i][j] - 3) / 4]);
-								Pie(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE,
-									START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * (CELL_SIZE / 2), START_X + (j + 1) * (CELL_SIZE / 2), START_Y + i * CELL_SIZE);
-								DeleteObject(hBrush);
-							}
-							else if ((closed_board[i][j] - 3) % 4 == 2) {
-								HBRUSH hBrush = CreateSolidBrush(pie_color[(closed_board[i][j] - 3) / 4]);
-								Pie(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE,
-									START_X + (j + 1) * (CELL_SIZE / 2), START_Y + (i + 1) * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * (CELL_SIZE / 2));
-								DeleteObject(hBrush);
-							}
-							else if ((closed_board[i][j] - 3) % 4 == 3) {
-								HBRUSH hBrush = CreateSolidBrush(pie_color[(closed_board[i][j] - 3) / 4]);
-								Pie(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE,
-									START_X + j * CELL_SIZE, START_Y + (i + 1) * (CELL_SIZE / 2), START_X + (j + 1) * (CELL_SIZE / 2), START_Y + (i + 1) * CELL_SIZE);
-								DeleteObject(hBrush);
-							}*/
-						}
-
-					}
 				}
 			}
 
-			if (mode == 0 && isDragging) {
-				HPEN hBluePen = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
-				HPEN oldPen = (HPEN)SelectObject(memDC, hBluePen);
-				HBRUSH oldBrush = (HBRUSH)SelectObject(memDC, GetStockObject(NULL_BRUSH));
+			// --- 2. 패들 그리기 ---
+			hBrush = CreateSolidBrush(RGB(100, 100, 255)); // 패들 색상 (파란색 계열)
+			oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
 
-				Rectangle(memDC, startPT.x, startPT.y, endPT.x, endPT.y);
+			Rectangle(memDC, (int)(paddle.pos.x - PADDLE_LEN / 2), (int)paddle.pos.y, (int)(paddle.pos.x + PADDLE_LEN / 2), (int)(paddle.pos.y + PADDLE_HEI));
 
-				SelectObject(memDC, oldBrush);
-				SelectObject(memDC, oldPen);
-				DeleteObject(hBluePen);
-			}
+			SelectObject(memDC, oldBrush);
+			DeleteObject(hBrush);
 
-			if (show_score) {
-				SetBkMode(memDC, TRANSPARENT);
-				SetTextColor(memDC, RGB(0, 0, 0));
-				SetTextAlign(memDC, TA_TOP);
-				_stprintf_s(str, L"Score : %d", score);
-				TextOut(memDC, START_X, START_Y + board_hei * CELL_SIZE + 20, str, _tcslen(str));
-			}
+			// --- 3. 공 그리기 ---
+			hBrush = CreateSolidBrush(RGB(255, 100, 100)); // 공 색상 (빨간색 계열)
+			oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
 
-			if (IsHint) {
-				for (int i = 0; i < board_len; ++i) {
-					for (int j = 0; j < board_hei; ++j) {
-						Rectangle(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE);
-
-						int x1 = START_X + j * CELL_SIZE;
-						int y1 = START_Y + i * CELL_SIZE;
-						int x2 = x1 + CELL_SIZE;
-						int y2 = y1 + CELL_SIZE;
-						int midX = x1 + CELL_SIZE / 2;
-						int midY = y1 + CELL_SIZE / 2;
-						if (closed_board[i][j] == 0) {
-							HBRUSH hBrush = CreateSolidBrush(RGB(230, 230, 230));
-							HBRUSH oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
-							Rectangle(memDC, START_X + j * CELL_SIZE, START_Y + i * CELL_SIZE, START_X + (j + 1) * CELL_SIZE, START_Y + (i + 1) * CELL_SIZE);
-							SelectObject(memDC, oldBrush);
-							DeleteObject(hBrush);
-						}
-						else if (closed_board[i][j] == 1) {
-							SetTextColor(memDC, RGB(255, 0, 0));
-							SetBkMode(memDC, TRANSPARENT);
-
-							HFONT hFont = CreateFont(CELL_SIZE * 0.75, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
-								DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-								DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial");
-							HFONT oldFont = (HFONT)SelectObject(memDC, hFont);
-
-							UINT oldAlign = SetTextAlign(memDC, TA_CENTER | TA_BASELINE);
-
-							TextOut(memDC, midX, midY + (int)(CELL_SIZE * 0.25), L"B", 1);
-
-							SetTextAlign(memDC, oldAlign);
-							SelectObject(memDC, oldFont);
-							DeleteObject(hFont);
-						}
-						else if (closed_board[i][j] == 2) {
-							HBRUSH hItemBrush = CreateSolidBrush(RGB(255, 255, 0));
-							HBRUSH oldItemBrush = (HBRUSH)SelectObject(memDC, hItemBrush);
-
-							POINT pts[4] = { { midX, y1 }, { x2, midY }, { midX, y2 }, { x1, midY } };
-
-							Polygon(memDC, pts, 4);
-
-							SelectObject(memDC, oldItemBrush);
-							DeleteObject(hItemBrush);
-						}
-						else if (closed_board[i][j] > 2) {
-							int val = closed_board[i][j] - 3;
-							int remainder = val % 4;
-							int colorIdx = val / 4;
-
-							HBRUSH hBrush = CreateSolidBrush(pie_color[colorIdx]);
-							HBRUSH oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
-
-							if (remainder == 0) Pie(memDC, x1, y1, x2, y2, midX, y1, x1, midY);
-							else if (remainder == 1) Pie(memDC, x1, y1, x2, y2, x2, midY, midX, y1);
-							else if (remainder == 2) Pie(memDC, x1, y1, x2, y2, midX, y2, x2, midY);
-							else if (remainder == 3) Pie(memDC, x1, y1, x2, y2, x1, midY, midX, y2);
-
-							SelectObject(memDC, oldBrush);
-							DeleteObject(hBrush);
-						}
-					}
-				}
-			}
+			if (isS)
+				Ellipse(memDC, (int)(ball.pos.x - BALL_RADIUS), (int)(ball.pos.y - BALL_RADIUS), (int)(ball.pos.x + BALL_RADIUS), (int)(ball.pos.y + BALL_RADIUS));
+			else
+				Ellipse(memDC, (int)(paddle.pos.x - BALL_RADIUS), (int)(paddle.pos.y - BALL_RADIUS * 2), (int)(paddle.pos.x + BALL_RADIUS), (int)paddle.pos.y);
+			SelectObject(memDC, oldBrush);
+			DeleteObject(hBrush);
 
 			// 완성된 백버퍼를 실제 화면으로 한 번에 복사
 			BitBlt(hDC, 0, 0, rt.right, rt.bottom, memDC, 0, 0, SRCCOPY);
@@ -344,25 +297,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam)) {
-		case ID_MENU_GAMESTART:
-			init_setting();
 
-			InvalidateRect(hWnd, NULL, FALSE);
-			break;
-		case ID_MENU_GAMEEND:
-			if (!game_end)
-				game_end = true;
-
-			InvalidateRect(hWnd, NULL, FALSE);
-			break;
-		case ID_MENU_HINT:
-			IsHint = true;
-			SetTimer(hWnd, 0, 1000, NULL);
-
-			InvalidateRect(hWnd, NULL, FALSE);
-			break;
-		case ID_MENU_SCORE:
-			show_score = !show_score;
 
 			InvalidateRect(hWnd, NULL, FALSE);
 			break;
@@ -372,63 +307,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		return 1;
 
 	case WM_LBUTTONDOWN:
-		if (game_end) break;
+	{
+		int mx = LOWORD(lParam);
+		int my = HIWORD(lParam);
 
-		isDragging = true;
-		startPT.x = LOWORD(lParam);
-		startPT.y = HIWORD(lParam);
-		endPT = startPT;
-
-		if (mode == 1) {
-			goto OPEN_CELL;
-		}
-		break;
+		if (mx >= paddle.pos.x - PADDLE_LEN / 2 && mx <= paddle.pos.x + PADDLE_LEN / 2 && my >= paddle.pos.y && my <= paddle.pos.y + PADDLE_HEI)
+			isPaddleDragging = true;
+	}
+	break;
 
 	case WM_MOUSEMOVE:
-		if (isDragging) {
-			endPT.x = LOWORD(lParam);
-			endPT.y = HIWORD(lParam);
+		if (isPaddleDragging) {
+			paddle.pos.x = (float)LOWORD(lParam);
 
-			if (mode == 1) {
-			OPEN_CELL:
-				int j = (endPT.x - START_X) / CELL_SIZE;
-				int i = (endPT.y - START_Y) / CELL_SIZE;
-				if (i >= 0 && i < board_hei && j >= 0 && j < board_len) {
-					if (opened_board[i][j] == 0) {
-						opened_board[i][j] = 1;
-						OpenCellEvent(hWnd, i, j, score);
-					}
-				}
-			}
-			InvalidateRect(hWnd, NULL, FALSE);
+			// 화면 경계 제한 (패들이 창 밖으로 나가지 않게)
+			if (paddle.pos.x < PADDLE_LEN / 2) paddle.pos.x = PADDLE_LEN / 2;
+			if (paddle.pos.x > rt.right - PADDLE_LEN / 2) paddle.pos.x = rt.right - PADDLE_LEN / 2;
 		}
 		break;
 
 	case WM_LBUTTONUP:
-		if (isDragging) {
-			if (mode == 0) {
-				int left = min(startPT.x, endPT.x);
-				int top = min(startPT.y, endPT.y);
-				int right = max(startPT.x, endPT.x);
-				int bottom = max(startPT.y, endPT.y);
-
-				for (int i = 0; i < board_hei; i++) {
-					for (int j = 0; j < board_len; j++) {
-						int cellX1 = START_X + j * CELL_SIZE;
-						int cellY1 = START_Y + i * CELL_SIZE;
-						int cellX2 = cellX1 + CELL_SIZE;
-						int cellY2 = cellY1 + CELL_SIZE;
-
-						if (!(cellX2 < left || cellX1 > right || cellY2 < top || cellY1 > bottom)) {
-							if (opened_board[i][j] == 0) {
-								opened_board[i][j] = 1;
-								OpenCellEvent(hWnd, i, j, score);
-							}
-						}
-					}
-				}
-			}
-			isDragging = false;
+		if (isPaddleDragging) {
+			isPaddleDragging = false;
 			InvalidateRect(hWnd, NULL, FALSE);
 		}
 		break;
@@ -437,9 +337,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		GetClientRect(hWnd, &rt);
 
 		if (wParam == 0) {
-			KillTimer(hWnd, 0);
+			float displacement = paddle.pos.x - lastPaddleX;
+			paddle.velocity = abs(displacement);
+			lastPaddleX = paddle.pos.x;
 
-			IsHint = false;
+			// 패들 화면 경계 제한
+			if (paddle.pos.x - PADDLE_LEN / 2.0f < rt.left)
+				paddle.pos.x = rt.left + PADDLE_LEN / 2.0f;
+			if (paddle.pos.x + PADDLE_LEN / 2.0f > rt.right)
+				paddle.pos.x = rt.right - PADDLE_LEN / 2.0f;
+
+			if (isS) {
+				ball.pos.x += ball.moving_pos.x;
+				ball.pos.y += ball.moving_pos.y;
+
+				// 벽 충돌 (좌/우)
+				if (ball.pos.x - BALL_RADIUS < rt.left) {
+					ball.pos.x = rt.left + BALL_RADIUS;
+					ball.moving_pos.x *= -1;
+				}
+				else if (ball.pos.x + BALL_RADIUS > rt.right) {
+					ball.pos.x = rt.right - BALL_RADIUS;
+					ball.moving_pos.x *= -1;
+				}
+
+				// 천장 충돌
+				if (ball.pos.y - BALL_RADIUS < rt.top) {
+					ball.pos.y = (float)rt.top + BALL_RADIUS;
+					ball.moving_pos.y *= -1;
+				}
+
+				// 바닥 충돌
+				if (ball.pos.y - BALL_RADIUS > rt.bottom) {
+					isS = false;
+					ball.moving_pos = { 0, 0 };
+					ball.pos.x = paddle.pos.x;
+					ball.pos.y = paddle.pos.y - BALL_RADIUS;
+				}
+
+				CollisionToPaddle();
+			}
+			else {
+				ball.pos.x = paddle.pos.x;
+				ball.pos.y = paddle.pos.y - BALL_RADIUS;
+			}
 			InvalidateRect(hWnd, NULL, FALSE);
 		}
 
@@ -447,7 +388,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		UpdateWindow(hWnd);
 		break;
 	case WM_DESTROY:
-		KillTimer(hWnd, 1);
+		KillTimer(hWnd, 0);
 		PostQuitMessage(0);
 		break;
 	}
